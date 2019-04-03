39347ff7e9d30c1c2321c552f9697c5b01c3c4c8
lic-w <lic-w@glodon.com>
2019-04-03 13:43:16 +0800

    Android调用JS代码 通过WebView的evaluateJavascript（） 并进行参数传递回调
    (cherry picked from commit 4c012f2d8fa6ca94104e3d21cbbcdccdec1a0bc8)
    
    # Conflicts:
    #	app/src/main/java/com/example/carson_ho/webview_demo/Android2JS2Activity.java

---
+++ app/src/main/java/com/example/carson_ho/webview_demo/Js2Android1Activity.java
@@ -0,0 +1,13 @@
+package com.example.carson_ho.webview_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Js2Android1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_js2_android1);
+    }
+}
---
+++ app/src/main/java/com/example/carson_ho/webview_demo/bean/AndroidtoJs.java
@@ -0,0 +1,11 @@
+package com.example.carson_ho.webview_demo.bean;
+
+/**
+ * 说明：
+ * Created by lic-w
+ *
+ * @email lic-w@glodon.com
+ * @date 2019/3/25 17:06
+ **/
+public class AndroidtoJs {
+}
